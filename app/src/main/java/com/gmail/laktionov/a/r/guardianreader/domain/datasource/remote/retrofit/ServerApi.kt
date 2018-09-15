0cf3b1919a16b1d44ab9b928359a8b21b6d92b64
Roman Laktionov <r.a.laktionov@gmail.com>
2018-09-15 16:28:26 +0300

    - single article response removed;

--- app/src/main/java/com/gmail/laktionov/a/r/guardianreader/domain/datasource/remote/retrofit/ServerApi.kt
+++ app/src/main/java/com/gmail/laktionov/a/r/guardianreader/domain/datasource/remote/retrofit/ServerApi.kt
@@ -2,13 +2,9 @@ package com.gmail.laktionov.a.r.guardianreader.domain.datasource.remote.retrofit
 
 import retrofit2.Call
 import retrofit2.http.GET
-import retrofit2.http.Path
 
 interface ServerApi {
 
     @GET("/search")
     fun getAllArticles(): Call<AllArticleResponse>
-
-    @GET("/{path}")
-    fun getSingleArticle(@Path(value = "path") path: String): Call<SingleArticleResponse>
 }
